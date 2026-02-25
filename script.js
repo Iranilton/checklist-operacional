105d0b660166dfbc1a555d5195f96a0617cbbb21
Iranilton <103905829+Iranilton@users.noreply.github.com>
2026-02-25 19:40:04 -0300

    Refactor trocarTipo and add entrar function

--- script.js
+++ script.js
@@ -10,3 +10,54 @@ document.getElementById("loginOperador").classList.remove("hidden")
 document.getElementById("loginUsuario").classList.remove("hidden")
 }
 }
+
+function entrar(){
+
+let tipo=document.getElementById("tipo").value
+
+if(tipo==="operador"){
+
+usuarioAtual=document.getElementById("nomeOperador").value
+tipoUsuario="operador"
+
+if(!usuarioAtual){
+alert("Digite seu nome")
+return
+}
+
+}else{
+
+let usuario=document.getElementById("usuario").value
+let senha=document.getElementById("senha").value
+
+// login simples de teste
+if(usuario==="manutencao" && senha==="123"){
+tipoUsuario="manutencao"
+usuarioAtual=usuario
+}else if(usuario==="gestao" && senha==="123"){
+tipoUsuario="gestao"
+usuarioAtual=usuario
+}else{
+alert("Usuário ou senha inválido")
+return
+}
+}
+
+document.getElementById("login").classList.add("hidden")
+
+if(tipoUsuario==="operador"){
+document.getElementById("operador").classList.remove("hidden")
+}
+
+if(tipoUsuario==="manutencao"){
+mostrarListaManutencao()
+mostrarListaGestao()
+document.getElementById("manutencao").classList.remove("hidden")
+document.getElementById("gestao").classList.remove("hidden")
+}
+
+if(tipoUsuario==="gestao"){
+mostrarListaGestao()
+document.getElementById("gestao").classList.remove("hidden")
+}
+}
